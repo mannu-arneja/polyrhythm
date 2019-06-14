44a1643df3ef927718f09dc09d6cc82a00209606
Mannu <mannu.arneja@gmail.com>
2019-06-14 17:24:57 -0400

    incorporate track sequencer

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
 window.addEventListener('load', init, false);
 function init() {
 
-  // init part state
-  // part.isPlaying = false;
-
   //resume from suspension - no autoplay default
   document.getElementById('play-button').addEventListener('click', () => {
     if (getAudioContext().state !== 'running') {
@@ -17,9 +14,7 @@ function init() {
       document.getElementById('play-button').value = 'pause'
     }
   });
-
-
-
+  
 }
 
 
@@ -34,6 +29,10 @@ let prevTime = 0;
 function setup() {
 
   createCanvas(620, 480);
+
+  // tracks
+  angle = radians(0);
+  fps = 30;
 
   // prepare the osc and env used by playNote()
   env = new p5.Envelope(0.01, 0.8, 0.2, 0);
@@ -99,20 +98,44 @@ function playSnare(time, params) {
   // part.start();
 // }
 
-// rotating sequencer
-let angle = 0.0
-let increment = 0
 
 function draw () {
-  background(51);
-  noStroke();
-  fill(255);
-  ellipseMode(CENTER);
+  background(220);
 
-  increment += radians(1)
 
-  translate(width/2, height/2);
-  rotate(increment);
-  ellipse(0, 0, 180, 180)
+  // origin
+  translate(width / 2, height / 2)
+
+  // rotation settings
+  frameRate(fps);
+  angle += radians(3);
+  rotate(angle);
+
+  //track settings
+  stroke(0);
+  strokeWeight(2);
+  fill(0, 130, 210, 255);
+
+  // 1/4 note track
+  arc(0, 0, 250, 250, radians(0), radians(90), PIE)
+  arc(0, 0, 250, 250, radians(90), radians(180), PIE)
+  arc(0, 0, 250, 250, radians(180), radians(270), PIE)
+  arc(0, 0, 250, 250, radians(270), radians(360), PIE)
+
+
+  fill(40, 130, 80, 255);
+  circle(0, 0, 200, 200)
+  // 1/3 note track
+  arc(0, 0, 200, 200, radians(0), radians(120), PIE)
+  arc(0, 0, 200, 200, radians(120), radians(240), PIE)
+  arc(0, 0, 200, 200, radians(240), radians(360), PIE)
+
+
+  fill(220);
+  circle(0, 0, 100, 100);
+
+
+  fill(220);
+  circle(0, 0, 100, 100);
 
 }
--- track.js
+++ track.js
@@ -1,24 +1,44 @@
 function setup() {
     createCanvas(400, 400);
     angle = radians(0);
-    fps = 60;
+    fps = 30;
 }
 
 function draw() {
     background(220);
-    frameRate(fps);
 
+    // origin
     translate(height / 2, width / 2)
-    angle += radians(1);
+
+    // rotation settings
+    frameRate(fps);
+    angle += radians(3);
     rotate(angle);
-    fill(0, 130, 210, 105);
-    circle(0, 0, 200, 200);
+
+    //track settings
     stroke(0);
     strokeWeight(2);
-    line(0, 0, 0, -100);
-    line(0, 0, 0, 100);
-    line(0, 0, -100, 0);
-    line(0, 0, 100, 0);
+    fill(0, 130, 210, 255);
+
+    // 1/4 note track
+    arc(0, 0, 250, 250, radians(0), radians(90), PIE)
+    arc(0, 0, 250, 250, radians(90), radians(180), PIE)
+    arc(0, 0, 250, 250, radians(180), radians(270), PIE)
+    arc(0, 0, 250, 250, radians(270), radians(360), PIE)
+
+
+    fill(40, 130, 80, 255);
+    circle(0, 0, 200, 200)
+    // 1/3 note track
+    arc(0, 0, 200, 200, radians(0), radians(120), PIE)
+    arc(0, 0, 200, 200, radians(120), radians(240), PIE)
+    arc(0, 0, 200, 200, radians(240), radians(360), PIE)
+
+
+    fill(220);
+    circle(0, 0, 100, 100);
+
+
     fill(220);
     circle(0, 0, 100, 100);
 }
